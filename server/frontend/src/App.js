9174f9521e10c4deba29ad93968ee501fa1b214c
Thinal-Fernando <r.thinal.fernando@gmail.com>
2026-02-28 00:17:32 -0500

    added review

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,22 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import Dealer from "./components/Dealers/Dealer"
+import Dealers from './components/Dealers/Dealers';
+
+
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/dealers" element={<Dealers/>} />
+
+
+
     </Routes>
   );
 }
